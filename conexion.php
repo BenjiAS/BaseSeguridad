d7dec8b48f6cef8e0e245ea5e21b9fcf2ec38d99
BenjiAS <BenjiAS@MacBook-Pro-de-BenjiAS.local>
2016-04-26 09:51:34 -0500

    Pruebas de Remote Repo

--- conexion.php
+++ conexion.php
@@ -1,10 +1,11 @@
 <?php
-	
+	//ESTOS SON COMMENTS PARA VER	
 	$db = new mysqli('localhost:3306', 'root','','seguridad');
 	if ($db-> connect_errno) {
 		//printf('Database connection failed miserably: %s\n', $mysqli->connect_error);
 		exit();
 	}
+	//SI FUNCIONA LO DE GIT PUSH Y PULL
 	else {
 		//printf('Sí se conectó');
 		//echo "<br>";
